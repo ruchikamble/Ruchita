bcd6f19c630054becda5200feba66a8bbfab6bd2
Ruchita Kamble <ruchita.kamble@capgemini.com>
2018-06-14 09:35:35 +0530

    14june

--- SpringBootBank/src/test/java/com/test/spring/SpringBootBank/TestCases.java
+++ SpringBootBank/src/test/java/com/test/spring/SpringBootBank/TestCases.java
@@ -4,12 +4,21 @@ import java.math.BigDecimal;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.test.spring.SpringBootBank.exception.BankException;
 import com.test.spring.SpringBootBank.pojo.Account;
 import com.test.spring.SpringBootBank.service.IAccountService;
 
+/*@RunWith(SpringRunner.class)
+@SpringBootTest
+@SpringBootConfiguration*/
+@ComponentScan("application.properties")
 public class TestCases 
 {
 	@Autowired
